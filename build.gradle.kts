c4ec33dfcaa308f59feae0955ee7915c06bce70e
Vitaly Sulimov <v.sulimov@fastmail.fm>
2023-12-02 00:18:28 +0500

    Create an IntelliJ IDEA project.
    Empty project with all necessary configurations for the further development.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "com.vsulimov"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
